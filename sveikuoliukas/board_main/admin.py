a0c93d23e4f88a302b4e029ad8edb7222c079677
egluzelu <egluze.lu@gmail.com>
2024-03-07 16:33:40 +0200

    patobulintas admin

--- sveikuoliukas/board_main/admin.py
+++ sveikuoliukas/board_main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from . import models
 
-# Register your models here.
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner']
+    list_display_links = ['name', 'owner']
+    list_filter = ['owner']
+    search_fields = ['name']
+    fieldsets = (
+        (_("general").title(), {
+            "fields": (
+                'name', 'owner',
+            ),
+        }),
+    )
+    
+
+admin.site.register(models.Board, BoardAdmin)
+
+
--- sveikuoliukas/board_main/models.py
+++ sveikuoliukas/board_main/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(_("name of the Board"), max_length=100, db_index=True)
+    description = models.TextField(_("description"), blank=True, max_length=100000)
+    owner = models.ForeignKey(
+        get_user_model(),
+        verbose_name=_("owner"),
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = _("board")
+        verbose_name_plural = _("board")
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("board_detail", kwargs={"pk": self.pk})
+    
